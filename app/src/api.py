6c4b8b4ea2c23af143f7cce800c40281b38d3a8f
BossyRomain <romain.bossy.01@gmail.com>
2024-11-27 13:53:03 +0100

    fix(triples): correction de l'ajout des membres des équipes des films dans le graph

--- app/src/api.py
+++ app/src/api.py
@@ -4,7 +4,7 @@ Module des appels aux différentes apis
 
 import requests
 
-from conf import url_themoviedb_api, themovedb_api_key
+from conf import url_themoviedb_api, themoviedb_api_key
 
 # Le cache pour les films (clé: imdb id)
 movies_cache = {}
@@ -27,18 +27,21 @@ def get_person(id):
     Retourne les informations d'une personne, charge la personne dans le cache si elle n'est pas présente
     """
     if not is_person_in_cache(id):
-        people_cache[id] = requests.get(url_themoviedb_api + "person/" + str(id) + "?api_key=" + themovedb_api_key).json()
+        people_cache[id] = requests.get(url_themoviedb_api + "/person/" + str(id) + "?api_key=" + themoviedb_api_key).json()
     return people_cache[id]
 
 
 def get_movie_crew(movie):
-    movie_id = requests.get(url_themoviedb_api + "find/" + movie['imdbID'] + "?external_source=imdb_id&api_key=" + themovedb_api_key).json()['movie_results'][0]['id']
+    try:
+        movie_id = requests.get(url_themoviedb_api + "/find/" + movie['imdbID'] + "?external_source=imdb_id&api_key=" + themoviedb_api_key).json()['movie_results'][0]['id']
 
-    movie_credits = requests.get(url_themoviedb_api + "movie/" + str(movie_id) + "/credits?api_key=" + themovedb_api_key).json()['cast']
-    
-    movie_crew = []
+        movie_credits = requests.get(url_themoviedb_api + "/movie/" + str(movie_id) + "/credits?api_key=" + themoviedb_api_key).json()['cast']
+        
+        movie_crew = []
 
-    for person in movie_credits:
-        movie_crew.append(get_person(person['id']))
+        for person in movie_credits:
+            movie_crew.append(get_person(person['id']))
 
-    return movie_crew
+        return movie_crew
+    except:
+        return []
